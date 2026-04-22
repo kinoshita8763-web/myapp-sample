a9a0d10cab079a8e8461d3b15fb9efbf5c845e53
y-kinoshita <y-kinoshita@NHP2W437>
2026-04-22 16:02:41 +0900

    プロジェクトファイルを追加。

---
+++ MyApp2.Test/CalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using MyApp2;
+using NUnit.Framework;
+
+namespace MyApp2.Test;
+
+public class CalculatorTests
+{
+    [Test]
+    public void Add_ReturnsCorrectSum()
+    {
+        var calc = new Calculator();
+        Assert.That(calc.Add(2, 3), Is.EqualTo(5));
+    }
+
+    [Test]
+    public void Subtract_ReturnsCorrectDifference()
+    {
+        var calc = new Calculator();
+        Assert.That(calc.Subtract(3, 2), Is.EqualTo(1));
+    }
+}
---
+++ MyApp2/Calculator.cs
@@ -0,0 +1,7 @@
+﻿namespace MyApp2;
+
+public class Calculator
+{
+    public int Add(int a, int b) => a + b;
+    public int Subtract(int a, int b) => a - b;
+}
